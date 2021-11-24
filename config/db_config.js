4209fd0b5b3451f67cf31cbb6c77e94e6fff6e23
Martin Grippo <mgrippo_11@hotmail.com>
2021-11-24 12:23:34 -0300

    coneccion a la DB

---
+++ config/db_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'https://phpmyadmin.alwaysdata.com',
+        user: 'C1G1',
+        password: 'Grupo01',
+        database: 'c1g1_empresa'
+    }
+};
