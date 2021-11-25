92e8b83d53cea8b537aa1b39a1358bfbe5052151
Martin Grippo <mgrippo_11@hotmail.com>
2021-11-25 20:24:38 -0300

    controlador post user

--- config/db_config.js
+++ config/db_config.js
@@ -2,7 +2,6 @@ module.exports = {
     database: {
         host: 'mysql-c1g1.alwaysdata.net',
         user: 'c1g1',
-        //server: 'mysql-c1g1.alwaysdata.net',
         password: 'Grupo01',
         database: 'c1g1_empresa'
     }
--- models/users_model.js
+++ models/users_model.js
@@ -1,15 +1,15 @@
-const poolDB = require('../config/db');
+//const poolDB = require('../config/db');
 
 const users = function(user) {
     this.id_usuario = user.id_usuario,
-    this.nombre = user.nombre,
-    this.mail = user.mail,
-    this.legajo = user.legajo,
-    this.password = user.password,
-    this.es_admin = user.es_admin,
-    this.empresa_id = user.empresa_id,
-    this.escritorio_id = user.escritorio_id,
-    this.edificio_id = user.edificio_id
+    this.nombre = user.nombre.STRING,
+    this.mail = user.mail.STRING,
+    this.legajo = user.legajo.INT,
+    this.password = user.password.STRING,
+    this.es_admin = user.es_admin.INT,
+    this.empresa_id = user.empresa_id.INT,
+    this.escritorio_id = user.escritorio_id.INT,
+    this.edificio_id = user.edificio_id.INT
 }
 
 module.exports = users;
