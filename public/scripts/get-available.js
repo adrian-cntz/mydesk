7d3ee95c087cef865174454258128cc4022411d0
adrian_cntz <adriancontizanetti@gmail.com>
2021-12-02 23:26:30 -0300

    Editada la vista 'Lista de usuarios'

--- public/scripts/get-available.js
+++ public/scripts/get-available.js
@@ -1,10 +1,11 @@
 //Este script se incluye en seleccionar-turno.ejs
 //Genera una lista con espacios disponibles para reservar en una determinada fecha
-var dateInput = date.value();
+var dateInput = date.value;
 //actualizar fecha seleccionada en variable dateInput
 date.addEventListener("change", function () {
   var dateInput = this.value;
 });
+var update = document.getElementById('updateList'); 
 update.addEventListener("click", function () {
   //convertir fecha al formato local
   var dmyDate = dateInput.split("/").reverse().join("-");
