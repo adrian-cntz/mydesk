7d0ca68f0d06af46b8321e694799e4bbffe6e0e3
Federico Hirsch Veloso <fhirsch@onfocusdev.com>
2023-03-29 12:18:00 -0300

    eslint

--- src/controllers/index_controller.js
+++ src/controllers/index_controller.js
@@ -1,37 +1,19 @@
-'use strict';
-const poolDB = require('../database/config/db');
+"use strict";
+const poolDB = require("../database/config/db");
 
+const home = async (req, res) => {
+  console.log(req.cookies.legajo);
+  const legajo = req.cookies.legajo;
+  const sql = `SELECT * from usuarios WHERE legajo = ${legajo}`;
 
-const home = async (req,res) => {
-    console.log(req.cookies.legajo)
-    const legajo = req.cookies.legajo;
-
-    const sql = `SELECT * from usuarios WHERE legajo = ${legajo}`;
-    
-    await poolDB.query(sql, (err, rows, fields) =>{
-        if(!err){
-            /*const sqlpuestos1 = `SELECT * from puestos WHERE tipo = 1 AND estado = 0`;
-            poolDB.query(sqlpuestos1, (err, p1, fields) =>{
-                if(!err){
-                    const pl1 = p1.length;
-                    const sqlpuestos2 = `SELECT * from puestos WHERE tipo = 2 AND estado = 0`;
-                    poolDB.query(sqlpuestos2, (err, p2, fields) =>{
-                        if(!err){
-                            const pl2 = p2.length;
-                            const sqlpuestos3 = `SELECT * from puestos WHERE tipo = 3 AND estado = 0`;
-                            poolDB.query(sqlpuestos3, (err, p3, fields) =>{
-                                if(!err){
-                                    const pl3 = p3.length;*/
-                                    
-                                res.render('./user/workspaces-list', {rows})
-                                }
-                                else{
-                                    console.error(err)
-                                }
-        })
-}
-
+  try {
+    const { rows } = await poolDB.query(sql);
+    res.render("./user/workspaces-list", { rows });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
-    home
-}
+  home,
+};
