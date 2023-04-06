a99231bb7ad1b776789c1113a336f01c077d4159
Federico Hirsch Veloso <fhirsch@onfocusdev.com>
2023-04-06 14:34:24 -0300

    correccion error eliminar + popup

--- src/controllers/puesto_controller.js
+++ src/controllers/puesto_controller.js
@@ -1,27 +1,17 @@
-'use strict';
-const poolDB = require('../database/config/db');
+"use strict";
+const poolDB = require("../database/config/db");
 
-const getlibres = async (req, res, next) => {
-    const tipo = req.params.tipo;
-    res.render("./user/seleccionar-turno");
-}
-
-
-//OBTENER TODOS
 const getAll = async (req, res, next) => {
-    const sql = 'SELECT * from puestos';
-    poolDB.query(sql, (err, rows, fields) =>{
-        if(!err){
-            res.render("./admin/place-list", {rows})
-        }
-        else{
-            console.error(err)
-        }
-    })
-}
-
+  const sql = "SELECT * from puestos";
+  poolDB.query(sql, (err, rows, fields) => {
+    if (!err) {
+      res.render("./admin/place-list", { rows });
+    } else {
+      console.error(err);
+    }
+  });
+};
 
 module.exports = {
-    getlibres,
-    getAll
-}
+  getAll,
+};
