b05d7637f0977f9e1396f76c59c8961d41027c20
Federico Hirsch Veloso <fhirsch@onfocusdev.com>
2023-04-06 14:32:19 -0300

    add firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChZpOmLfbcEnmxAn5uNVvhGT0gXUOnK2s",
+  authDomain: "mydesk-f8b29.firebaseapp.com",
+  projectId: "mydesk-f8b29",
+  storageBucket: "mydesk-f8b29.appspot.com",
+  messagingSenderId: "247594116492",
+  appId: "1:247594116492:web:e455e9ab8e171e333262a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
